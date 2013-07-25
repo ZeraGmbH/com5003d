46fa9deb1dbe97616b71d55e9045d09bb14b84d3
Peter Lohmer <peterlohmer01@gmail.com>
2013-07-25 10:03:59 +0200

    application.h: bugfix
    Signed-off-by: Peter Lohmer <peterlohmer01@gmail.com>

--- application.h
+++ application.h
@@ -11,11 +11,14 @@ public:
     cApplication(int &argc, char** argv)
         :QCoreApplication(argc, argv)
     {
-        QTimer::singleShot(0, this, SIGNAL(appStarting()));
+        QTimer::singleShot(0, this, SLOT(StartApp()));
     }
 
 signals:
     void appStarting();
+
+private slots:
+    void StartApp(){emit appStarting();};
 };
 
 #endif // APPLICATION_H
