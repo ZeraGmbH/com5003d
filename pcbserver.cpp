66c4beeab5a3a8b759755ed3559600a68e4a541f
Peter Lohmer <peterlohmer01@gmail.com>
2013-07-30 11:18:15 +0200

    bugfix
    Signed-off-by: Peter Lohmer <peterlohmer01@gmail.com>

--- pcbserver.cpp
+++ pcbserver.cpp
@@ -52,7 +52,7 @@ void cPCBServer::executeCommand(const QByteArray cmd)
     cSCPIObject* scpiObject;
     QString dummy;
 
-    QTextCodec* codec = QTextCodec::codecForName("UTF-8");
+    QTextCodec* codec = QTextCodec::codecForUtfText(cmd);
     m_sInput = codec->toUnicode(cmd);
     qDebug() << m_sInput;
     if ( (scpiObject =  m_pSCPInterface->getSCPIObject(m_sInput, dummy)) != 0)
