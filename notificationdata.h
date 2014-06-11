dcd77c1c1584c4f67dfed7a2cb894435fa0ecb15
Peter Lohmer <peterlohmer01@gmail.com>
2014-06-11 12:31:48 +0200

    notificationdata.h: new class for keeping notification info together
    Signed-off-by: Peter Lohmer <peterlohmer01@gmail.com>

--- notificationdata.h
+++ notificationdata.h
@@ -1,4 +1,14 @@
 #ifndef NOTIFICATIONDATA_H
 #define NOTIFICATIONDATA_H
 
+#include <QByteArray>
+#include <protonetpeer.h>
+
+struct cNotificationData
+{
+    ProtoNetPeer *netClient;
+    QByteArray clientID;
+    quint16 notifier;
+};
+
 #endif // NOTIFICATIONDATA_H
