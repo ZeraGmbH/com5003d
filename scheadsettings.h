beb626e2259833652ad469ceed1a58b7796125e3
Peter Lohmer <peterlohmer01@gmail.com>
2015-03-06 11:42:42 +0100

    scheadsettings.h,cpp: added xml configuration support for scanning heads
    Signed-off-by: Peter Lohmer <peterlohmer01@gmail.com>

--- scheadsettings.h
+++ scheadsettings.h
@@ -1,4 +1,59 @@
 #ifndef SCHEADSETTINGS_H
 #define SCHEADSETTINGS_H
 
+
+#include <QObject>
+#include <QList>
+
+#include "xmlsettings.h"
+
+namespace SCHeadSystem
+{
+enum configstate
+{
+    cfgSH0Alias,
+    cfgSH0muxchannel,
+    cfgSH0avail,
+};
+
+
+struct cChannelSettings // what we want to get configured
+{
+    QString m_sAlias; // the names channel
+    quint8 m_nmuxChannel; // where to configure the channel
+    bool avail; // is this channel available ?
+};
+}
+
+
+class QString;
+
+namespace Zera
+{
+namespace XMLConfig
+{
+    class cReader;
+}
+}
+
+
+class cSCHeadSettings : public cXMLSettings
+{
+    Q_OBJECT
+
+public:
+    cSCHeadSettings(Zera::XMLConfig::cReader *xmlread);
+    ~cSCHeadSettings();
+    QList<SCHeadSystem::cChannelSettings*>& getChannelSettings();
+
+
+public slots:
+    virtual void configXMLInfo(QString key);
+
+private:
+    QList<SCHeadSystem::cChannelSettings*> m_ChannelSettingsList;
+};
+
+
+
 #endif // SCHEADSETTINGS_H
